eb30e28ec3276dd6b9739c3420bb0bfbcb1ba399
Jannis Born <jab@zurich.ibm.com>
2020-09-15 11:42:42 +0200

    bump: torch dependency

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setup(
     ),
     install_requires=[
         'paccmann_predictor @ git+https://github.com/PaccMann/paccmann_predictor',
-        'torch', 'deepchem', 'tensorflow<2.0', 'Pillow', 'six',
-        # 'rdkit'
+        'torch', 'deepchem', 'tensorflow<2.0', 'Pillow', 'six'
     ],
     packages=find_packages('.'),
     zip_safe=False
