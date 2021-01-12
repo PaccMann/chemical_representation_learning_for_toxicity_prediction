1c1ed2dddad0a10f0ee0cba940776b964746014d
Jannis Born <jab@zurich.ibm.com>
2021-01-12 16:27:43 +0100

    wip: Params for RNN added

--- toxsmi/models/__init__.py
+++ toxsmi/models/__init__.py
@@ -1,6 +1,7 @@
 from .mca import MCAMultiTask
 from .dense import Dense
 from .cnn import CNN
+from .rnn import RNN
 
 # More models could follow
-MODEL_FACTORY = {'mca': MCAMultiTask, 'dense': Dense, 'cnn': CNN}
+MODEL_FACTORY = {'mca': MCAMultiTask, 'dense': Dense, 'cnn': CNN, 'rnn': RNN}
