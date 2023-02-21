f5a0ed7bb37339b43ca4b8bf8d3e4ebe407b276e
jannisborn <jab@zurich.ibm.com>
2023-02-21 10:13:29 +0100

    feat: nan-ignore wrapper for regression

--- toxsmi/utils/hyperparams.py
+++ toxsmi/utils/hyperparams.py
@@ -1,10 +1,14 @@
 from paccmann_predictor.utils.hyperparams import LOSS_FN_FACTORY
 
-from .wrappers import BCEIgnoreNaN
+from .wrappers import BCEIgnoreNaN, GenericIgnoreNaN
 
 LOSS_FN_FACTORY.update(
     {
         "binary_cross_entropy_ignore_nan_and_sum": BCEIgnoreNaN("sum"),
         "binary_cross_entropy_ignore_nan_and_mean": BCEIgnoreNaN("mean"),
+        "mse_ignore_nan_and_mean": GenericIgnoreNaN(loss="mse", reduction="mean"),
+        "mse_ignore_nan_and_sum": GenericIgnoreNaN(loss="mse", reduction="sum"),
+        "l1_ignore_nan_and_mean": GenericIgnoreNaN(loss="l1", reduction="mean"),
+        "l1_ignore_nan_and_sum": GenericIgnoreNaN(loss="l1", reduction="sum"),
     }
 )
